3eb1a42cec284194a633fc8218c28d69b74d6e8e
MusazadeNihat1 <78012026+MusazadeNihat1@users.noreply.github.com>
2025-08-05 19:40:51 +0400

    Create main.js

---
+++ function/main.js
@@ -0,0 +1,6 @@
+const burgerCheckbox = document.getElementById("burger");
+const navbar = document.getElementById("navbar");
+
+burgerCheckbox.addEventListener("change", () => {
+    navbar.classList.toggle("active", burgerCheckbox.checked);
+});
